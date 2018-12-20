b03e56f33abbde105310de638f5259660bde483f
Easter Bohanon <eastdabeast@email.arizona.edu>
2018-12-20 12:54:39 -0700

    making the foundations

---
+++ logic.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyATv-13Oj7-HaxaQNnED5KkkObi0E7fbXg",
+    authDomain: "train-schedule-eab03.firebaseapp.com",
+    databaseURL: "https://train-schedule-eab03.firebaseio.com",
+    projectId: "train-schedule-eab03",
+    storageBucket: "train-schedule-eab03.appspot.com",
+    messagingSenderId: "688942696683"
+  };
+  firebase.initializeApp(config);
+  
