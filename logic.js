314b835fde52884c28ab075ec1c32e2be42acb77
Easter Bohanon <eastdabeast@email.arizona.edu>
2019-01-10 19:07:54 -0700

    finished Js, just need it to work

--- logic.js
+++ logic.js
@@ -8,4 +8,66 @@ var config = {
   };
   firebase.initializeApp(config);
   
-  var databse = firebase.databse();
+  var database = firebase.database();
+//  Making my variable to get this train running
+  // name = "";
+  // destination = "";
+  // time = "";
+  // frequency = 0;
+
+  $("#submit").on("click", function(){
+    event.preventDefault();
+
+    trainName = $("#TrainName").val().trim();
+    trainDestination = $("#Destination").val().trim();
+    trainTime= $("#Time").val().trim();
+    trainFreq = $("#Frequency").val().trim();
+    // making sure those values are being grabbed
+    console.log(trainName, trainDestination, trainTime, trainFreq);
+
+    // creates new variable so we can push 
+    var newTrain = {
+      name: trainName,
+      place: trainDestination,
+      time: trainTime,
+      frequency: trainFreq
+    };
+    
+    database.ref().push(newTrain);
+
+    console.log(newTrain.name);
+    console.log(newTrain.place);
+    console.log(newTrain.time);
+    console.log(newTrain.frequency);
+
+    alert("Train succesfully added");
+
+    $("#TrainName").val("");
+    $("#Destination").val("");
+    $("#Time").val("");
+    $("#Frequency").val("");
+
+
+  });
+
+  database.ref().on("child_added",function(childSnapshot){
+    console.log(childSnapshot.val()); 
+    var trainName = childSnapshot.val().name;
+    var trainDestination  = childSnapshot.val().place;
+    var trainTime = childSnapshot.val().time;
+    var trainFreq = childSnapshot.val().frequency;
+
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(trainTime);
+    console.log(trainFreq);
+   
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(trainDestination),
+      $("<td>").text(trainFreq),
+      $("<td>").text(trainTime)
+    );
+    
+    $("#table > tbody").append(newRow);
+  });
